0b14e3071ac81f1a5bbe9d37247c88936f618960
Dariusz Gawron <screbsy+g1@gmail.com>
2020-05-17 12:34:30 +0200

    Create Static Components

---
+++ src/Components/App/App.js
@@ -0,0 +1,47 @@
+import React from 'react';
+//import logo from './logo.svg';
+import './App.css';
+import { Playlist } from '../Playlist/Playlist';
+import { SearchBar } from '../SearchBar/SearchBar';
+import { SearchResults } from '../SearchResults/SearchResults';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+          <SearchBar />
+          <div className="App-playlist">
+            <SearchResults />
+            <Playlist />
+          </div>
+        </div>
+      </div>
+    )
+  };
+}
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+        
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+export default App;
+//<img src={logo} className="App-logo" alt="logo" />
